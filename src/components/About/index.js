e3fab7f65bcc53db99c65d675c7ad93232917c98
vaishnavi-dev955 <vaishnavimandala1998@gmail.com>
2023-03-07 16:20:54 +0000

    commit message

---
+++ src/components/About/index.js
@@ -0,0 +1,17 @@
+import './index.css'
+import Header from '../Header'
+
+const About = () => (
+  <>
+    <Header />
+    <div className="About1-container">
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/about-lg-img.png "
+        alt="about"
+        className="About-image"
+      />
+    </div>
+  </>
+)
+
+export default About
---
+++ src/components/Home/index.js
@@ -0,0 +1,17 @@
+import './index.css'
+import Header from '../Header'
+
+const Home = () => (
+  <>
+    <Header />
+    <div className="Home1-container">
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/home-lg-img.png"
+        alt="home"
+        className="Home-image"
+      />
+    </div>
+  </>
+)
+
+export default Home
